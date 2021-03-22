ffdc947a6d43ea8d9d28ef57c491a0d410c2b96c
Miguel Gamboa <miguelgamboa@outlook.com>
2021-03-22 21:28:30 +0000

    aula 4 Logger version 1

---
+++ aula03-reflection/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+namespace TCP02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Carregar dinamicamente os metadados da DLL. Usa caminho absoluto
+            //Assembly fileToRead = Assembly.LoadFile("D:\\Particular\\ISEL\\Ano 2 Sem 4\\AVE\\GIT\\AVE_homework\\TPC02\\RestSharp.dll");
+            Assembly fileToRead = Assembly.LoadFrom("RestSharp.dll");
+
+            //Tipoe e Metodos
+            foreach (Type type in fileToRead.GetTypes())
+            {
+                Console.WriteLine(type.Name);
+
+                foreach (MethodInfo method in type.GetMethods())
+                {
+                    Console.WriteLine("\t" + method.Name);
+                }
+            } 
+        }
+    }
+}
+/* 
+    Assembly    --> loadFrom()     GetTypes()   
+    Type        --> GetMethods()   GetFields()  GetField(string)
+
+    .net System.Reflection
+    e.g: obj.GetType().GetMethod("hello").Invoke(foo, null)
+         obj.GetType().GetMethod("hello").GetParameters())
+*/
---
+++ aula04-reflection-logger/Logger/Log.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+namespace Logger
+{
+    public class Log
+    {
+        public void Info(Object target) {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(target.GetType().Name);
+            builder.Append("{");
+            /**
+             * Get information of members to log
+             */
+            builder.Append(LogFields(target));
+            builder.Append(LogParameterlessMethods(target));
+            /**
+             * Finish output formatting
+             */
+            if(builder.Length != 0) builder.Length-= 2;
+            builder.Append("}");
+            /**
+             * Print to Console
+             */
+            Console.WriteLine(builder.ToString());
+        }
+        public string LogFields(Object target) {
+            StringBuilder builder = new StringBuilder();
+            // Inspect Fields
+            foreach(FieldInfo f in target.GetType().GetFields()) {
+                builder.Append(f.Name);
+                builder.Append(':');
+                builder.Append(f.GetValue(target));
+                builder.Append(", ");
+            }
+            return builder.ToString();
+        }
+        public string LogParameterlessMethods(Object target) {
+            StringBuilder builder = new StringBuilder();
+            // Inspect Fields
+            foreach(MethodInfo m in target.GetType().GetMethods()) {
+                if(m.GetParameters().Length == 0) {
+                    builder.Append(m.Name);
+                    builder.Append(':');
+                    builder.Append(m.Invoke(target, null));
+                    builder.Append(", ");
+                }
+            }
+            return builder.ToString();
+        }
+    }
+}
