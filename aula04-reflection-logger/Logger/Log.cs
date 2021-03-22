f0579009a7ba69147cdb134538f6ced6cc8b5ebe
Miguel Gamboa <miguelgamboa@outlook.com>
2021-03-22 21:45:37 +0000

    aula 4 Logger: version 2

--- aula04-reflection-logger/Logger/Log.cs
+++ aula04-reflection-logger/Logger/Log.cs
@@ -13,8 +13,7 @@ namespace Logger
             /**
              * Get information of members to log
              */
-            builder.Append(LogFields(target));
-            builder.Append(LogParameterlessMethods(target));
+            builder.Append(LogMembers(target));
             /**
              * Finish output formatting
              */
@@ -25,29 +24,36 @@ namespace Logger
              */
             Console.WriteLine(builder.ToString());
         }
-        public string LogFields(Object target) {
+        public string LogMembers(Object target) {
             StringBuilder builder = new StringBuilder();
             // Inspect Fields
-            foreach(FieldInfo f in target.GetType().GetFields()) {
-                builder.Append(f.Name);
-                builder.Append(':');
-                builder.Append(f.GetValue(target));
-                builder.Append(", ");
-            }
-            return builder.ToString();
-        }
-        public string LogParameterlessMethods(Object target) {
-            StringBuilder builder = new StringBuilder();
-            // Inspect Fields
-            foreach(MethodInfo m in target.GetType().GetMethods()) {
-                if(m.GetParameters().Length == 0) {
+            foreach(MemberInfo m in target.GetType().GetMembers()) {
+                if(ShoudlLog(m)) {
                     builder.Append(m.Name);
                     builder.Append(':');
-                    builder.Append(m.Invoke(target, null));
+                    builder.Append(GetValue(m, target));
                     builder.Append(", ");
                 }
             }
             return builder.ToString();
+        }
+        private bool ShoudlLog(MemberInfo m)
+        {
+            return (m.MemberType == MemberTypes.Field) 
+                || (m.MemberType == MemberTypes.Method 
+                    && ((MethodInfo) m).GetParameters().Length == 0);
+        }
+
+        private object GetValue(MemberInfo m, object target)
+        {
+            switch(m.MemberType) {
+                case MemberTypes.Field:
+                    return ((FieldInfo) m).GetValue(target);
+                case MemberTypes.Method:
+                    return ((MethodInfo) m).Invoke(target, null);
+                default:
+                    throw new InvalidOperationException("Member should not be logged!");
+            }
         }
     }
 }
