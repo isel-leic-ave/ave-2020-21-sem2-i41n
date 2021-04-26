f99f75d8383451d6713f79bc047b6bfd78d70f2e
Miguel Gamboa <miguelgamboa@outlook.com>
2021-04-26 22:24:35 +0100

    aula 15

---
+++ aula14-intermediate-language/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace aula14_intermediate_language
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Module (3,7) = " + Module(3,7));
+        }
+
+        /// <summary>
+        /// Receives a point coordinates and returns its module.
+        /// </summary>
+        static double Module(int x, int y){  
+            int x2 = x * x;
+            int y2 = y * y;
+            return Math.Sqrt(x2 + y2);
+        }
+
+    }
+}
