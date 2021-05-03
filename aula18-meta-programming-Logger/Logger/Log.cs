2a88abda3e7dc6d7fa0978bb7edeaa7c7b6abd62
Miguel Gamboa <miguelgamboa@outlook.com>
2021-05-03 21:33:01 +0100

    Aula 18 setup

---
+++ aula18-meta-programming-Logger/Logger/Log.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Logger
+{
+    public class Log : AbstractLog
+    {
+        public Log()
+        {
+        }
+
+        public Log(IPrinter p) : base(p)
+        {
+        }
+
+        
+
+        public override IEnumerable<IGetter> GetMembers(Type t)
+        {
+            // First check if exist in members dictionary
+            List<IGetter> ms;
+            if(!members.TryGetValue(t, out ms)) {
+                ms = new List<IGetter>();
+                foreach(MemberInfo m in t.GetMembers()) {
+                    IGetter getter = null; 
+                    if(ShoudlLog(m, out getter)) {
+                        ms.Add(getter);
+                    }
+                }
+                members.Add(t, ms);
+            }
+            return ms;
+        }
+    }
+}
---
+++ aula18-meta-programming-Logger/Logger/LogDynamic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Logger
+{
+    public class LogDynamic : AbstractLog
+    {
+        public LogDynamic()
+        {
+        }
+
+        public LogDynamic(IPrinter p) : base(p)
+        {
+        }
+
+        
+
+        public override IEnumerable<IGetter> GetMembers(Type t)
+        {
+            // First check if exist in members dictionary
+            List<IGetter> ms;
+            if(!members.TryGetValue(t, out ms)) {
+                ms = new List<IGetter>();
+                foreach(MemberInfo m in t.GetMembers()) {
+                    IGetter getter = null; 
+                    if(ShoudlLog(m, out getter)) {
+                        // 1. Create the class that implements IGetter for that member m in domain type t.
+                        // 2. Instantiate the class created on 1.
+                        // ms.Add(getter);
+                    }
+                }
+                members.Add(t, ms);
+            }
+            return ms;
+        }
+    }
+}
