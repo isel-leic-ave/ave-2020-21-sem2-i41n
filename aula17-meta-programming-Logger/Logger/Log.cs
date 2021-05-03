9040996939c71c9211b2445114d1ad351707f296
Miguel Gamboa <miguelgamboa@outlook.com>
2021-05-03 21:15:35 +0100

    Refactor Log to AbstractLog with a single abstract method GetMembers()

--- aula17-meta-programming-Logger/Logger/Log.cs
+++ aula17-meta-programming-Logger/Logger/Log.cs
@@ -6,70 +6,19 @@ using System.Text;
 
 namespace Logger
 {
-    public class Log
+    public class Log : AbstractLog
     {
-        private readonly IPrinter printer;
-        private readonly Dictionary<Type, List<IGetter>> members = new Dictionary<Type, List<IGetter>>();
-
-        public Log(IPrinter p )
+        public Log()
         {
-            printer = p;
         }
 
-        public Log() : this(new ConsolePrinter())
+        public Log(IPrinter p) : base(p)
         {
-            
-        }
-        public void Info(Object target) {
-            // option 1: String output = typeof(IEnumerable).IsAssignableFrom(target.GetType())
-            // option 2: String output = target is IEnumerable
-            // option 3
-            IEnumerable seq = target as IEnumerable;
-            String output = seq != null
-                ? Inspect(seq)
-                : Inspect(target);
-            printer.Print(output);
         }
 
-        public string Inspect(IEnumerable seq) {
-            StringBuilder builder = new StringBuilder();
-            builder.Append("Array of: ");
-            builder.Append("\n");
-            foreach(object item in seq) {
-                builder.Append("\t");
-                builder.Append(Inspect(item));
-                builder.Append("\n");
-            }
-            return builder.ToString();
-        }
-        public string Inspect(Object target) {
-            StringBuilder builder = new StringBuilder();
-            builder.Append(target.GetType().Name);
-            builder.Append("{");
-            /**
-             * Get information of members to log
-             */
-            builder.Append(LogMembers(target));
-            /**
-             * Finish output formatting
-             */
-            builder.Append("}");
-            return builder.ToString();
-        }
-        public string LogMembers(Object target) {
-            StringBuilder builder = new StringBuilder();
-            // Inspect Fields
-            foreach(IGetter m in GetMembers(target.GetType())) {
-                builder.Append(m.GetName());
-                builder.Append(':');
-                builder.Append(m.GetValue(target));
-                builder.Append(", ");
-            }
-            if(builder.Length != 0) builder.Length-= 2;
-            return builder.ToString();
-        }
+        
 
-        private IEnumerable<IGetter> GetMembers(Type t)
+        public override IEnumerable<IGetter> GetMembers(Type t)
         {
             // First check if exist in members dictionary
             List<IGetter> ms;
@@ -84,38 +33,6 @@ namespace Logger
                 members.Add(t, ms);
             }
             return ms;
-        }
-
-        private bool ShoudlLog(MemberInfo m, out IGetter getter)
-        {   
-            getter = null;
-            ///
-            /// Check if ToLog annotation exists
-            /// 
-            if(!Attribute.IsDefined(m,typeof (ToLogAttribute))) return false;
-            ///
-            /// Check if is a field or parameterless method
-            /// 
-            if(m.MemberType == MemberTypes.Field) {
-                getter = new GetterField((FieldInfo) m);
-                return true;
-            }
-            if(m.MemberType == MemberTypes.Method) {
-                MethodInfo mi = (MethodInfo) m;
-                if(mi.GetParameters().Length == 0) {
-                    getter = new GetterMethod((MethodInfo) m);
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        private class ConsolePrinter : IPrinter
-        {
-            public void Print(string output)
-            {
-                Console.WriteLine(output);
-            }
         }
     }
 }
